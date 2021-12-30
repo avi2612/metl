6db21e39afb6d3885e6a622867a5829b1bf39bf3
avi2612 <geekyavinash@gmail.com>
2021-12-30 19:54:35 +0000

    Revert "First Commit"
    This reverts commit c2112c43b220250306171e491cff9033080879aa.
    
    Conflicts:
    	.classpath
    	pom.xml
    	src/main/java/com/kkhindigyan/org/client/ClientTest.java
    	src/main/resources/applicationContext.xml

--- src/main/java/com/kkhindigyan/org/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.kkhindigyan.org.service;
-
-import com.kkhindigyan.org.model.Account;
-import com.kkhindigyan.org.service.exception.AccountNotFoundException;
-
-public interface AccountService {
-	public abstract Account updateAccountBalance(Account account, Long amount) throws AccountNotFoundException;
-
-}
--- src/main/java/com/kkhindigyan/org/service/exception/AccountNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.kkhindigyan.org.service.exception;
-
-public class AccountNotFoundException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public AccountNotFoundException(String message) {
-		super(message);
-	}
-}
